8c2c28d3ab5305e3e86870bb9f5cc1d0a113a84e
Simba6886-maker <krajsimha6886@gmail.com>
2025-10-25 11:48:02 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xj959wCu0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2fg9rgv33ds7ifrle4vqfvbqnm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_rHzom6cAJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1vgdk0brl1v2d66lpma5rek7ro', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
